51ecbad53c4e5b5b954cbbde6be968d4a80fe87f
denis.bled@newmips.com <denis.bled@newmips.com>
2023-06-02 13:38:48 +0200

    Tracks

--- public/js/room.js
+++ public/js/room.js
@@ -99,7 +99,7 @@ $( document ).ready(function() {
 
 function addRemoteVideo(stream, participantID) {
 
-	console.log('addRemoteVideo');
+	console.log('addRemoteVideo : ' + stream);
     var $videoBox = $(`<div class="row mt-4"><div class="col-12"><div class='videoWrap' id='uuid_`+participantID+`'><video onclick="selectVideo('`+participantID+`');" class="videoBox" autoplay="true" playsinline="true" muted="true"></video><button onclick="selectVideo('`+participantID+`');" class="btn btn-outline-secondary mt-2"><i class="fa-solid fa-camera"></i></button>&nbsp;&nbsp;<button onclick="selectAudio('`+participantID+`', this);" class="btn btn-outline-secondary mt-2 btn-audio"><i class="fa-solid fa-microphone"></i></button></div></div></div>`);
     // var $video = $(``);
     
