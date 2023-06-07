4bdf72aa20f5e3489052fce26d42d2cebe8adb6c
denis.bled@newmips.com <denis.bled@newmips.com>
2023-06-07 07:24:43 +0200

    Video Webm

--- public/js/recorder.js
+++ public/js/recorder.js
@@ -61,7 +61,7 @@ const downloadButton = document.querySelector('button#download');
 }); */
 downloadButton.addEventListener('click', () => {
 
-  saveAs(new Blob(recordedBlobs, {type: "application/octet-stream ;charset=utf-8"}), 'rushd.webm')
+  saveAs(new Blob(recordedBlobs, {type: 'video/webm'}), 'rushd.webm');
 });
 
 
